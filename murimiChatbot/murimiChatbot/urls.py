3cd63e01af9a47dc949493d90e89e329500f1ccf
Bunbee <barnabas@velocityinc.tech>
2023-05-16 13:34:24 +0200

    Add changes

--- murimiChatbot/murimiChatbot/urls.py
+++ murimiChatbot/murimiChatbot/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from murimi.views import ProductListCreateView, ProductRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', ProductListCreateView.as_view(), name='product-list-create'),
+    path('products/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(), name='product-retrieve-update-destroy')
 ]
