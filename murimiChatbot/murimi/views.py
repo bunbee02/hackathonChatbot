ecff086c6db9007eafa8d9796d09ba2d6f471ff9
Bunbee <barnabas@velocityinc.tech>
2023-05-19 14:09:41 +0200

    Add plants

--- murimiChatbot/murimi/views.py
+++ murimiChatbot/murimi/views.py
@@ -33,67 +33,10 @@ info =info
 from .models import CropDisease
 CropDisease =CropDisease
 
-account_sid = 'AC37cebed5e43eb81f4c0345dda4da8a4d'
-auth_token = '337348d00cd743158b37ac536c4fa7d9'
-client = Client(account_sid, auth_token)
-
-# @csrf_exempt
-# def bot(request): 
-#     print(request.POST.get('Body', ''))
-    # message = request.POST['Body']
-    # print(request)
-    # print(message)
-    # if (message == 'hi'):
-    #     try: 
-    #         print(message)
-    #         client.messages.create(
-    #         from_='whatsapp:+14155238886',
-    #         body='Welcome to Murimi bot please select the region you are coming',
-    #         to='whatsapp:+263713872372'
-    #         )
-    #     except TwilioRestException as ex: 
-    #         print(ex)
-
-    # return HttpResponse("hello")
 
 
-# def farming_practices_view(request, province_name, crop_name):
-#     try:
-#         province = get_object_or_404(Province, name=province_name)
-#         crop = get_object_or_404(Crop, name=crop_name)
-        
-#         if request.method == 'GET':
-#             farming_practices = FarmingPractice.objects.filter(province=province, crop=crop)
-#             practices_list = [practice.practice for practice in farming_practices]
-#             response_data = {
-#                 'province': province.name,
-#                 'crop': crop.name,
-#                 'farming_practices': practices_list
-#             }
-#             return JsonResponse(response_data)
 
-#         elif request.method == 'POST':
-#             practice = request.POST.get('practice')
-#             if practice:
-#                 FarmingPractice.objects.create(province=province, crop=crop, practice=practice)
-#                 return JsonResponse({'success': 'Farming practice added successfully'})
-#             else:
-#                 return JsonResponse({'error': 'Invalid farming practice'}, status=400)
 
-#         elif request.method == 'PUT':
-#             new_practice = request.PUT.get('practice')
-#             farming_practice = get_object_or_404(FarmingPractice, province=province, crop=crop)
-#             farming_practice.practice = new_practice
-#             farming_practice.save()
-#             return JsonResponse({'success': 'Farming practice updated successfully'})
-
-#         elif request.method == 'DELETE':
-#             farming_practice = get_object_or_404(FarmingPractice, province=province, crop=crop)
-#             farming_practice.delete()
-#             return JsonResponse({'success': 'Farming practice deleted successfully'})
-
-#     except (Province.DoesNotExist, Crop.DoesNotExist):
-#         return JsonResponse({'error': 'Province or crop not found'}, status=404)
 
 
 class ProductListCreateView(generics.ListCreateAPIView):
@@ -107,15 +50,6 @@ class ProductRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProductSerializer
     
 
-# class DatasetView(APIView):
-#     def get(self, request, format=None):
-#         # Load your dataset and convert it into Python objects
-#         dataset = load_dataset()
-        
-#         # Serialize the dataset
-#         serializer = DatasetSerializer(dataset, many=True)
-        
-#         return Response(serializer.data)
 
 
 def info_list(request):
@@ -226,17 +160,288 @@ def crop_disease_list(request):
 
 @csrf_exempt
 def bot(request): 
+  
     prompt = request.POST["Body"]
     # sender_name = request.POST['ProfileName']
     # message = ''
-    print(prompt)
+    resp = {}
 
     if prompt == 'hi':
-     message= client.messages.create(
-           from_='whatsapp:+14155238886',
-           body='Welcome to Murimi bot please select the region you are coming',
-           to='whatsapp:+263713872372'
+        message= client.messages.create(
+        from_='whatsapp:+14155238886',
+        body='''Welcome to Murimi bot please select the region you are coming
+        1. Mashonaland Central
+        2. Mashonaland East
+        3. Mashonaland West
+        4. Matebeleland North
+        5. Matebeleland South
+        6. Harare
+        7. Manicaland
+        8. Midlands
+        9. Masvingo
+        10. Bulawayo. 
+        ''',
+        to='whatsapp:+263713872372'
         )
-    # print(message.sid)
-    return HttpResponse("hello")
+    if prompt == '1':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Mashonaland Central and in this province these are the crops that you can plant
+            maize
+            tobbacco
+            cotton
+            soyabeans
+            groundnuts
+            cabbages''',
+        to='whatsapp:+263713872372'
+        )
+
+    if prompt == '2':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Mashonaland East and in this province these are the crops that you can plant
+            maize
+            tobbacco
+            cotton
+            sugarcane
+            wheat
+            cabbages''',
+        to='whatsapp:+263713872372'
+        )
+
+    if prompt == '3':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Mashonaland West and in this province these are the crops that you can plant
+            maize
+            tobbacco
+            cotton
+            sugarcane
+            groundnuts
+            ''',
+        to='whatsapp:+263713872372'
+        )
+
+    if prompt == '7':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Manicaland and in this province these are the crops that you can plant
+            maize
+            tea
+            cotton
+            soya beans
+            sunflower
+            wheat
+            ''',
+        to='whatsapp:+263713872372'
+        )
+
+    if prompt == '8':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Midlands and in this province these are the crops that you can plant
+            maize
+            wheat
+            soyabeans
+            sunflower
+            cotton
+            ''',
+        to='whatsapp:+263713872372'
+        )
+    if prompt == '3':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Mashonaland West and in this province these are the crops that you can plant
+            maize
+            tobbacco
+            cotton
+            sugarcane
+            groundnuts
+            ''',
+        to='whatsapp:+263713872372'
+        )
+    if prompt == '9':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Masvingo and in this province these are the crops that you can plant
+            maize
+            sugarcane
+            cotton
+            citrus fruits
+            groundnuts
+            ''',
+        to='whatsapp:+263713872372'
+        )
+    if prompt == '4':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Matebeleland North West and in this province these are the crops that you can plant
+            maize
+            sorghum
+            cotton
+            sugarcane
+            groundnuts
+            ''',
+        to='whatsapp:+263713872372'
+        )
+    if prompt == '6':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Harare and in this province these are the crops that you can plant
+            maize
+            wheat
+            potatoes
+            vegetables
+            ''',
+        to='whatsapp:+263713872372'
+        )
+    if prompt == '5':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Matebeleland South and in this province these are the crops that you can plant
+            maize
+            sorghum
+            wheat
+            cotton
+            sunflower
+            ''',
+        to='whatsapp:+263713872372'
+        )
+
+    if prompt == '10':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Bulawayo and in this province these are the crops that you can plant
+            maize
+            tobbacco
+            cotton
+            sugarcane
+            groundnuts
+            ''',
+        to='whatsapp:+263713872372'
+        )
+    if prompt == 'maize':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Maize
+                Choose the right variety: Select a maize variety that is well-suited for your climate, soil type, and intended use (e.g., sweet corn, field corn, or popcorn). Consider factors such as disease resistance, yield potential, and maturity length.
+
+                Prepare the soil: Maize thrives in well-drained, fertile soil. Start by clearing the area of weeds and debris. Loosen the soil using a garden fork or tiller. Incorporate organic matter, such as compost or well-rotted manure, to improve soil fertility and structure.
+
+                Plan the planting time: Maize is a warm-season crop and requires soil temperatures of at least 50°F (10°C) for germination. The exact planting time will depend on your location and climate. In general, plant maize after the last frost date in your area and when the soil has warmed up.
+
+                Sow the seeds:
+
+                Dig furrows or trenches in the prepared soil, spaced about 2-3 feet apart, to allow for proper plant spacing and airflow.
+                Plant the maize seeds at a depth of about 1-2 inches, placing them 6-12 inches apart within the row.
+                Cover the seeds with soil and gently firm it down.
+                                
+                These dressings break down after 21 days so there can be a risk where there is slow emergence of seed in cold, wet seed-beds.
+            ''',
+        to='whatsapp:+263713872372'
+        )
+    if prompt == 'tobacco':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Tobbaco
+           Legal Considerations:
+
+        Research and comply with the legal requirements and regulations for tobacco cultivation in your region. Obtain any necessary permits or licenses.
+        Familiarize yourself with any specific restrictions or regulations related to selling, processing, or manufacturing tobacco.
+        Choose the Right Variety:
+
+        Select a tobacco variety suitable for your climate and soil conditions. Different tobacco types include flue-cured, burley, oriental, and cigar wrapper tobacco. Each type has specific requirements and characteristics.
+        Soil Preparation:
+
+        Tobacco requires well-drained, fertile soil. Conduct a soil test to assess the nutrient composition and pH levels of your soil.
+        Prepare the soil by removing weeds and debris. Till the soil to a depth of around 6-8 inches, incorporating organic matter such as compost or well-rotted manure.
+        Adjust the soil pH to the recommended range for your chosen tobacco variety.
+        Seedbed Preparation:
+                    ''',
+        to='whatsapp:+263713872372'
+        )
+    if prompt == 'cotton':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Bulawayo and in this province these are the crops that you can plant
+            limate and Soil:
+
+        Cotton requires a warm climate, with temperatures ranging between 60°F (15°C) and 95°F (35°C) during the growing season.
+        Choose a location with well-drained soil. Cotton thrives in loamy or sandy soil with good moisture retention. The soil should have a pH between 5.8 and 7.0.
+        Variety Selection:
+
+        Select cotton varieties that are suitable for your region and climate. Consider factors such as disease resistance, yield potential, fiber quality, and maturity length.
+        Consult local agricultural extension services or seed suppliers for recommendations on suitable cotton varieties.
+                    ''',
+        to='whatsapp:+263713872372'
+        )
+    if prompt == 'soyabeans':
+              message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Bulawayo and in this province these are the crops that you can plant
+            maize
+            tobbacco
+            cotton
+            sugarcane
+            groundnuts
+            ''',
+        to='whatsapp:+263713872372'
+        )
+              
+    if prompt == 'groundnuts':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Bulawayo and in this province these are the crops that you can plant
+            maize
+            tobbacco
+            cotton
+            sugarcane
+            groundnuts
+            ''',
+        to='whatsapp:+263713872372'
+        )
+    if prompt == 'wheat':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Bulawayo and in this province these are the crops that you can plant
+            maize
+            tobbacco
+            cotton
+            sugarcane
+            groundnuts
+            ''',
+        to='whatsapp:+263713872372'
+        )
+
+    if prompt == 'sunflower':
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=''' You selected Bulawayo and in this province these are the crops that you can plant
+            maize
+            tobbacco
+            cotton
+            sugarcane
+            groundnuts
+            ''',
+        to='whatsapp:+263713872372'
+        )
+
+        return HttpResponse("hello")
+        
+    # elif prompt.lower == "mashonaland":
+    #     message = client.messages.create(
+    #     from_='whatsapp:+14155238886',
+    #     body='You selected mashonalad',
+    #     to='whatsapp:+263713872372'
+    #     )
+    #     return HttpResponse("hello")
+
+
+    
+
+        # print(message.sid)
+    else: 
+        print('did not work')
+        return HttpResponse('No file')
+
    
